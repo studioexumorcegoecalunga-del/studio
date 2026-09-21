6d1ce1f39c8c1764f2eee4af2319253ade7c5035
studioexumorcegoecalunga-del <studioexumorcegoecalunga@gmail.com>
2026-09-21 10:07:02 -0300

    feat: structural phase 39 V2 production adapter and versioned results - src/RofamaCad/Structural/SolverV2Adapter.cs

---
+++ src/RofamaCad/Structural/SolverV2Adapter.cs
@@ -0,0 +1,7 @@
+namespace RofamaCad.Structural;
+internal static class SolverV2Adapter
+{
+ public static UnifiedBeamSolverV2.Result Solve(double[] spans,double[] ei,double[] q,bool[] fixV,bool[] fixR,ReleaseModelService.Flags[] releases,IReadOnlyList<UnifiedBeamSolver.PointLoad> points){
+  var rel=releases.Select(x=>new UnifiedBeamSolverV2.Release(x.Start,x.End)).ToArray();var p=points.Select(x=>new UnifiedBeamSolverV2.PointLoad(x.Element,x.X,x.P)).ToArray();return UnifiedBeamSolverV2.Solve(spans,ei,q,fixV,fixR,rel,p);
+ }
+}
